481a8b95a5990964b6f1b7c409fb447d07082e1a
ajleech1139-eng <ajleech1139@gmail.com>
2025-12-14 21:50:03 -0500

    Login page finished, sign up started

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String _Email = '';
+    String _Password = '';
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -46,31 +48,105 @@ class MyApp extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: Column(
               children: [
+
                 //Email
                 TextField(
                   decoration: InputDecoration(
+                    
                     labelText: 'login',
                   ),
+                  onChanged: (value) => _Email = value,
                 ),
 
-                const SizedBox(height: 60),
+                const SizedBox(height: 40),
 
                 // password
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
+                Password(
+                  onChanged: (value) => _Password = value,
                 ),
+
+                const SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      onPressed: null,    // null is temporary
+                      child: const Text('Login'),
+                    ),
+
+                    const SizedBox(width: 20),
+
+                    ElevatedButton(
+                      onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpPage()));
+                      },
+                      child: const Text('sign up'),
+                    ),
+                  ],
+                  ),
               ],
             )
             )
-
           ],
         )
-
-
       )
     );
   }
 }
+
+
+class Password extends StatefulWidget {
+  final ValueChanged<String>? onChanged;
+
+  const Password({super.key, this.onChanged});
+
+  @override
+  State<Password> createState() => _PasswordState();
+}
+
+class _PasswordState extends State<Password> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility_off : Icons.visibility,
+          ),
+          onPressed: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+        )
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 97, 25, 20),
+          title: Text(
+            'CherryPick🍒',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+            ),
+            centerTitle: true,
+      ),
+    );
+  }
+}
+
